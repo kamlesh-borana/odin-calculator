2ecbf9209451227dc11267400a01c0a33cff3624
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-29 19:28:46 +0530

    Add function for deciding the math operation
    Added operate function which decides based on the operator passed which
    math operation is to be performed.

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, operand1, operand2) {
+    switch(operator) {
+        case "+":
+            return add(operand1, operand2);
+        
+        case "-":
+            return subtract(operand1, operand2);
+
+        case "*":
+            return multiply(operand1, operand2);
+
+        case "/":
+            return divide(operand1, operand2);
+
+        default:
+            return "Invalid Operator";
+    }
+}

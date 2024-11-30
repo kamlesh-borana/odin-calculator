2f3821981ecbb6ba1085ad00c36dd2d3756b8a4d
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-30 12:33:52 +0530

    Add logic to perform basic calculation
    Added logic to make the digit buttons i.e ("0-9" and ".") work to update
    the display and operands and perform simple calculator operation using
    buttons for addition, subtraction, multiplication, division and equal.
    Also handled scenarios for the following
    1. Not allowing multiple 0 at beginning
    2. Not allowing more than one "."
    3. Wait for second operand input to allow operator switching in between
    4. Handle clicking operator and equal buttons without proper inputs
    5. Perform calculation when operator button is pressed when both
    operands are present and set the operator and result as first operand

--- script.js
+++ script.js
@@ -32,3 +32,113 @@ function operate(operator, operand1, operand2) {
             return "Invalid Operator";
     }
 }
+
+let operand1 = null;
+let operand2 = null;
+let operator = null;
+let displayText = "";
+let currentOperand = 1;
+
+const displayDiv = document.querySelector(".display");
+
+function showDisplayText() {
+    displayDiv.textContent = displayText || "0";
+}
+
+function updateDisplayText(text) {
+    if(displayText === "0") {
+        displayText = text;
+    } else if(text === "." && displayText.includes(".")) {
+        return;
+    } else {
+        displayText = displayText + text;
+    }
+}
+
+function updateOperand1(value) {
+    operand1 = Number(value);
+}
+
+function updateOperand2(value) {
+    operand2 = Number(value);
+}
+
+function updateOperand() {
+    if(currentOperand === 1) {
+        updateOperand1(displayText);
+    } else {
+        updateOperand2(displayText);
+    }
+}
+
+const digitButtons = document.querySelectorAll(".digit");
+digitButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        updateDisplayText(event.target.value);
+        updateOperand();
+        showDisplayText();
+    });
+});
+
+function updateOperator(operatorSign) {
+    operator = operatorSign;
+}
+
+function toggleOperand() {
+    currentOperand = currentOperand === 1 ? 2 : 1;
+}
+
+function resetOperand2() {
+    operand2 = null;
+}
+
+function resetOperator() {
+    operator = null;
+}
+
+function resetDisplayText() {
+    displayText = "";
+}
+
+function calculateAndShowResult() {
+    const result = operate(operator, operand1, operand2);
+    resetDisplayText();
+    updateDisplayText(String(result));
+    toggleOperand();
+    updateOperand();
+    showDisplayText();
+    resetOperand2();
+    resetOperator();
+}
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        if(isNaN(operand1) || isNaN(operand2)) {
+            return;
+        }
+
+        if(operator && displayText === "") {
+            updateOperator(event.target.value);
+            return;
+        }
+
+        if(operator && (operand1 != null) && (operand2 != null)) {
+            calculateAndShowResult();
+        }
+
+        updateOperator(event.target.value);
+        toggleOperand();
+        updateOperand();
+        resetDisplayText();
+    });
+});
+
+const equalButton = document.querySelector(".equal");
+equalButton.addEventListener("click", () => {
+    if(!operator || (operand1 == null) || isNaN(operand1) || (operand2 == null) || isNaN(operand2) || (displayText === "")) {
+        return;
+    }
+
+    calculateAndShowResult();
+});
